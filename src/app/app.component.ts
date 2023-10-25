5e50a9e3fed2744eae2607eb77715f14506a081b
Ahuvi112 <ahuvigit@gmail.com>
2023-10-25 15:51:37 +0300

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  cityName: string = 'Israel';
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherDate(this.cityName)
+  }
+
+  onSubmit() {
+    this.getWeatherDate(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherDate(cityName: string) {
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(response);
+        }
+      });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    // weatherApiBaseUrl: 'https://open-weather-map27.p.rapidapi.com/weather',
+    // XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    // XRapidAPIHostHeaderValue: 'open-weather-map27.p.rapidapi.com',
+    // XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    // XRapidAPIKeyHeaderValue: '8a4b198e70msh58b2e605c81d875p1b477ajsneed3afddba6b',
+    weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5/weather',
+    APPID: '8fe3e3af576d784063a2ea40462ae0ef',
+}
